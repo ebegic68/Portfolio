63b196464d999d645ba075338c5e9dabc09c5fa5
ebegic68 <119817064+ebegic68@users.noreply.github.com>
2023-08-20 14:17:42 +0200

    Društvena mreža
    Mali projekt društvene mreže, sličan društvenoj mreži Twitter. Registracija, postavljanje postova, lajkanje.

---
+++ Društvena_mreža/inc/footer.php
@@ -0,0 +1,12 @@
+<div class="footer">
+    <hr>
+    <p>&copy Copyright - 2022</p>
+    <form class="autor">
+        <p>The author is Eldin Begić</p>
+    </form>
+</div>
+<script src="js/sctripts.js"></script>
+
+</div>
+</body>
+</html>
